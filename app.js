27a32b5d150580f6c3ba968691932713654c7ebd
r4coon77 <139823112+r4coon77@users.noreply.github.com>
2023-10-19 17:25:46 +0100

    added ComputerSelection variable w This variable calls getComputerChoice Function

--- app.js
+++ app.js
@@ -10,17 +10,16 @@ const scissors = "Scissors";
 
 function getComputerChoice(computerChoice) {
     if (computerChoice === 1) {
-        console.log(rock)
         return rock;
     } else if (computerChoice === 2) {
-        console.log(paper)
         return paper;
     } else
-        console.log(scissors)
     return scissors;
 }
 
-getComputerChoice(Math.floor((Math.random() * 3) + 1))
+let computerSelection = getComputerChoice(Math.floor((Math.random() * 3) + 1))
+
+
 
 
 
