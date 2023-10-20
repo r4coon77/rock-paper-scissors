b4e8fd65309deafaa58277925bc53390fcdab33b
r4coon77 <139823112+r4coon77@users.noreply.github.com>
2023-10-20 12:11:20 +0100

    Added game function This function compares player/computer selection of rock, paper or scissors and consoles logs winner

--- app.js
+++ app.js
@@ -8,16 +8,52 @@ const rock = "Rock";
 const paper = "Paper";
 const scissors = "Scissors";
 
+function getPlayerChoice(playerChoice) {
+    if (playerChoice === rock) {
+        return rock;
+    } else if (playerChoice === paper) {
+        return paper;
+    } else
+        return scissors;
+}
+
 function getComputerChoice(computerChoice) {
     if (computerChoice === 1) {
         return rock;
     } else if (computerChoice === 2) {
         return paper;
     } else
-    return scissors;
+        return scissors;
 }
 
-let computerSelection = getComputerChoice(Math.floor((Math.random() * 3) + 1))
+function game(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log("Player Wins!");
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log("Computer Wins!")
+    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
+        console.log("Draw")
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log("Player Wins!");
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log("Computer Wins!")
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        console.log("Draw")
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("Player Wins!");
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log("Computer Wins!")
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        console.log("Draw")  
+}
+}
+
+let playerSelection = getPlayerChoice(paper);
+let computerSelection = getComputerChoice(Math.floor((Math.random() * 3) + 1));
+
+game(playerSelection, computerSelection)
+
+
 
 
 
